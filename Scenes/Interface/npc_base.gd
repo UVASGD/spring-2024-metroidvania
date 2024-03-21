8916e873de623efe981c644db94330ca712db79d
kedrickfudala <KedrickAFudala@gmail.com>
2024-03-21 16:22:54 -0400

    Added support for interactable NPCs

--- Scenes/Interface/npc_base.gd
+++ Scenes/Interface/npc_base.gd
@@ -1,25 +1,36 @@
 extends Sprite2D
 class_name NPCBase
 
-@export var dialogue : String
-
 @onready var interact_icon : Object = $InteractIcon
-@onready var can_interact : bool = false
+@onready var interactable_area : Object = $InteractableArea
+@onready var text_box : Object = $TextBox
+@onready var text_speed : Object = $TextSpeed
+@onready var text_timeout : Object = $TextTimeout
+
+@onready var is_speaking : bool = false
+@export var dialogue : String = "This is a test string!"
 
 func _ready():
 	interact_icon.visible = false
+	text_speed.set_wait_time(.05) # time between letters
+	text_timeout.set_wait_time(4)
+	text_box.clear()
 
 func _process(delta):
 	pass
 
-func _on_area_2d_area_entered(area):
-	if(area.name == "InteractionArea"):
-		print("I can interact!")
-		interact_icon.visible = true
-		can_interact = true
+func interact():
+	if !is_speaking:
+		text_box.clear()
+		speak_text(dialogue)
 
-func _on_area_2d_area_exited(area):
-	if(area.name == "InteractionArea"):
-		print("I cannot interact :()")
-		interact_icon.visible = false
-		can_interact = false
+func speak_text(text : String):
+	is_speaking = true
+	for letter in text:
+		text_speed.start()
+		text_box.add_text(letter)
+		await text_speed.timeout
+	is_speaking = false
+	text_timeout.start()
+	await text_timeout.timeout
+	text_box.clear()
--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -7,13 +7,20 @@ class_name Player
 @onready var anim_player : Object = $AnimationPlayer
 @onready var sprite : Object = $Sprite2D
 @onready var coyote_time : Object = $CoyoteJumpTimer
-@onready var interaction_area : Object = $InteractionArea
+@onready var interaction_area : Object = $InteractArea
 
 @onready var max_health : int = 100
 @onready var current_health : int = max_health
 
 @export var SPEED : float = 1000.0
 @export var VELOCITY : float = 0.0
+
+@onready var is_dashing : bool = false
+@onready var is_attacking : bool = false
+
+@onready var interactable_area_count : int = 0
+@onready var is_interacting : bool = false
+@onready var can_interact : bool = false
 
 func _ready():
 	pass
@@ -25,13 +32,30 @@ func _physics_process(delta):
 	do_movement(delta)
 
 func get_inputs():
-	SPEED = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	
-	if(is_on_floor() and Input.is_action_just_pressed("jump")):
-		jump()
+	if !is_interacting:
+		SPEED = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 
-	if(Input.is_action_just_pressed("attack1")):
-		print("I attack!")
+		if !is_dashing:
+			if is_on_floor() && Input.is_action_just_pressed("jump"):
+				jump()
+
+			if Input.is_action_just_pressed("dash"):
+				pass
+
+			if Input.is_action_just_pressed("attack1"):
+				print("I attack!")
+
+			if Input.is_action_just_pressed("interact") && interactable_area_count > 0:
+				var closest_area = null
+				var closest_distance = 100
+				for area in get_tree().get_nodes_in_group("InteractableAreas"):
+					if(area.global_position.distance_to(global_position) < closest_distance):
+						closest_distance = area.global_position.distance_to(global_position)
+						closest_area = area
+				if(closest_area != null):
+					var parent = closest_area.get_parent()
+					parent.interact()
+					print("I am now interacting!")
 
 ######################################## MOVEMENT FUNCTIONS ########################################
 func do_movement(delta):
@@ -47,6 +71,21 @@ func jump():
 ######################################### COMBAT FUNCTIONS #########################################
 
 
+
+
+
+####################################### INTERACTION FUNCTIONS ######################################
+func _on_interact_area_area_entered(area):
+	if(area.name == "InteractableArea"):
+		area.add_to_group("InteractableAreas")
+		area.get_parent().interact_icon.visible = true
+		interactable_area_count += 1
+
+func _on_interact_area_area_exited(area):
+	if(area.name == "InteractableArea"):
+		area.remove_from_group("InteractableAreas")
+		area.get_parent().interact_icon.visible = false
+		interactable_area_count -= 1
 
 ######################################## SPRITE/ANIMATIONS #########################################
 func update_anims(input_axis):
@@ -70,3 +109,6 @@ func update_anims(input_axis):
 func flip_sprite(input_axis):
 	sprite.flip_h = input_axis < 0
 	sprite.position.x = input_axis * 2
+
+
+
